cd3ffb6ec689e0fa462891e47990be90ceed2735
Heyang Wang <heyang@Heyangs-MacBook-Pro.local>
2017-06-06 23:26:19 -0400

    more concise

--- 11Container With Most Water.java
+++ 11Container With Most Water.java
@@ -3,6 +3,23 @@ public class Solution {
         int lo = 0, hi = height.length - 1;
         int area = (hi - lo) * Math.min(height[lo], height[hi]);
         while(lo <= hi && lo <= height.length - 1){
+            area = Math.max(area, (hi - lo) * height[hi]);
+            if(height[lo] >= height[hi])
+                hi--;
+            else
+                lo++;
+        }
+        return area;
+    }
+}
+
+
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int lo = 0, hi = height.length - 1;
+        int area = (hi - lo) * Math.min(height[lo], height[hi]);
+        while(lo <= hi && lo <= height.length - 1){
             if(height[lo] >= height[hi]){
                 area = Math.max(area, (hi - lo) * height[hi]);
                 hi--;
