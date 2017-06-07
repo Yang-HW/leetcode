713a6499806e17e783cc4dd5f49cd58c2f6de342
Heyang Wang <heyang@Heyangs-MacBook-Pro.local>
2017-06-06 23:21:46 -0400

    add leetcode11

---
+++ 11Container With Most Water.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int lo = 0, hi = height.length - 1;
+        int area = (hi - lo) * Math.min(height[lo], height[hi]);
+        while(lo <= hi && lo <= height.length - 1){
+            if(height[lo] >= height[hi]){
+                area = Math.max(area, (hi - lo) * height[hi]);
+                hi--;
+            }else if(height[lo] < height[hi]){
+                area = Math.max(area, (hi - lo) * height[lo]);
+                lo++;
+            }
+        }
+        return area;
+    }
+}
